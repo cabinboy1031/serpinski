4946f69a747f64c62ad070cc4421be48994ee908
cabinboy1031 <enchantingMenace@gmail.com>
2023-05-26 20:10:14 -0700

    Added .gitignore

--- build/serpinski_test_include-b12d07c.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/home/ian/Projects/serpinski/build/serpinski_test_tests-b12d07c.cmake")
-  include("/home/ian/Projects/serpinski/build/serpinski_test_tests-b12d07c.cmake")
-else()
-  add_test(serpinski_test_NOT_BUILT-b12d07c serpinski_test_NOT_BUILT-b12d07c)
-endif()
